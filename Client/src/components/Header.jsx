a24687e4eff2b086b80c9d93d7bd16c47f1c185b
ANISH KUMAR <105239403+YadavAnish123@users.noreply.github.com>
2024-09-30 22:36:13 +0530

    all signup functionality added

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -4,13 +4,13 @@ import Home from '../pages/Home'
 
 const Header=()=> {
   return (
-    <header className='bg-slate-200 shadow-md'> 
+    <header className='bg-slate-700 shadow-md'> 
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'> 
     <Link to='/'> 
         <h1 className='font-bold text-sm sm:text-xl'>
              
-            <span className='px-1 text-slate-500'>Satyam</span>
-            <span className='px-1 text-slate-700'>Estate</span>
+            <span className='px-1 text-slate-200'>Satyam</span>
+            <span className='px-1 text-white'>Estate</span>
         </h1>
     </Link>
        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
@@ -19,13 +19,16 @@ const Header=()=> {
         </form>
         <ul className='flex gap-4'>
             <Link to='/'> 
-           <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+           <li className='hidden sm:inline text-white hover:underline'>Home</li>
            </Link>
             <Link to='/about'> 
-           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+           <li className='hidden sm:inline text-white hover:underline'>About</li>
            </Link>
            <Link to='/sign-in'> 
-           <li className=' sm:inline text-slate-700 hover:underline px-2'>Sig in</li>
+           <li className=' sm:inline text-white hover:underline px-2'>Sign in</li>
+           </Link>  
+           <Link to='/sign-up'> 
+           <li className=' sm:inline text-white hover:underline px-2'>Sign Up</li>
            </Link>    
         </ul> 
         </div>
--- Client/src/pages/SignUP.jsx
+++ Client/src/pages/SignUP.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUP=()=> {
+  const [formData,setformData]=useState({})
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const onchangehandler=(e)=>{
+    setformData({
+      ...formData,
+      [e.target.id]:e.target.value
+    })
+  }
+
+  console.log(formData)
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+     
+   const res= await fetch('/api/auth/signup',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(formData)
+    })
+    const data=await res.json();
+    console.log(data);
+  }
   return (
     <div className='p-3 max-w-lg mx-auto py-8'> 
       <h1 className='font-semibold text-center py-8'>SignUP</h1>
       <form className='flex flex-col gap-4'>
-        <input className='border p-3 rounded-lg' type="text" placeholder='username' id='username' />
-        <input className='border p-3 rounded-lg' type="email" placeholder='email' id='email' />
-        <input className='border p-3 rounded-lg' type="password" placeholder='password' id='password'/> 
-        <button className='bg-slate-700 p-3 rounded-lg text-white hover:opacity-90' type='button' id='button'>submit</button>
+        <input className='border p-3 rounded-lg' type="text" placeholder='username' id='name'  onChange={onchangehandler}/>
+        <input className='border p-3 rounded-lg' type="email" placeholder='email' id='email'  onChange={onchangehandler}/>
+        <input className='border p-3 rounded-lg' type="password" placeholder='password' id='password' onChange={onchangehandler}/> 
+        <button className='bg-slate-700 p-3 rounded-lg text-white hover:opacity-90' type='button' id='button' onClick={handleSubmit}>submit</button>
+        <span className='hover:underline text-center'>Already have an account? <Link className='text-cyan-700' to={'/sign-in'}>Login</Link></span>
       </form>
     </div>
   )
